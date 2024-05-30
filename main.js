902d1f8b079dc7f45f998b93b740e49ffd9ea353
Nanda <nandayavanets@mail.ru>
2024-05-30 19:39:42 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const isiTeks = [
+    {
+        prologue : "Alkisah ada sebuah kastil yang dihuni manusia kerdil bernama Andrej.",
+
+        cerita : "Sang pemain kemudian masuk ke dalam kastil, namun usahanya sia-sia."
+    }
+]
+
+
+
+
+// const divText = document.querySelector(".text").innerHTML = isiTeks[0].cerita;
+
