0af2a5aa94c32b089f3e2b858a3831cca93306f2
Nanda <nandayavanets@mail.ru>
2024-05-31 14:02:42 +0700

    add animation to opening

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-const isiTeks = [
-    {
-        prologue : "Alkisah ada sebuah kastil yang dihuni manusia kerdil bernama Andrej.",
+const opening = document.querySelector(".opening");
 
-        cerita : "Sang pemain kemudian masuk ke dalam kastil, namun usahanya sia-sia."
-    }
-]
-
-
+function touchOpening() {
+    opening.style.visibility = "hidden";
+    opening.style.opacity = "0";
+    opening.style.transition = "visibility 0s 2s, opacity 2s linear";
+}
 
 
-// const divText = document.querySelector(".text").innerHTML = isiTeks[0].cerita;
 
