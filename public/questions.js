762be1657908c8cce3a056d91d8e60a086517cf3
Nanda <nandayavanets@mail.ru>
2024-06-01 20:22:19 +0700

    add questions

---
+++ public/questions.js
@@ -0,0 +1,23 @@
+export const questions = [
+    {
+        levelSatu: 
+            {
+                pertanyaanSatu: "We've got ____ water left!",
+                pertanyaanDua: "Where ____ you come from?",
+                pertanyaanTiga: "What time does she ___ up?",
+                pertanyaanEmpat: "I am not ____ this film.",
+                pertanyaanLima: "I am seeing her ____ three o'clock.",
+                pertanyaanEnam: "Easter is ___ March this year.",
+                pertanyaanTujuh: "She's ____ work all day today.",
+                pertanyaanDelapan: "I go ___ by bus.",
+                pertanyaanSembilan: "Where ____ you come from?",
+                pertanyaanSepuluh: "Where ____ you come from?"
+            },
+
+        levelDua:
+            {
+                pertanyaanSatu: "he did it ___ her own.",
+                pertanyaanDua: "It is not ____ mine."
+            }
+    }
+]
