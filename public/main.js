d666239acd574ca2a00151b40cf06dd62392d8f6
Nanda <nandayavanets@mail.ru>
2024-06-01 20:22:03 +0700

    add text and button to prologue

--- public/main.js
+++ public/main.js
@@ -1,17 +1,37 @@
 import { isiTeks } from "./story.js";
+import { questions } from "./questions.js";
 
 const cerita = document.querySelector(".text");
 const opening = document.querySelector(".opening");
-
+const choices = document.querySelector(".choices");
 opening.addEventListener("touchstart", function() {
-    opening.style.visibility = "hidden";
-    opening.style.opacity = "0";
-    opening.style.transition = "visibility 0s 2s, opacity 2s linear";
-
-    cerita.innerHTML = isiTeks[0].prologue;
+    // opening.style.transition = "visibility 0s 2s, opacity 1s linear";
+    // opening.style.opacity = "0";
+    opening.style.display = "none";
+    const prologue = document.createElement("p");
+    prologue.innerText = isiTeks[0].prologue;
+    cerita.appendChild(prologue);
     cerita.style.textJustify = "inter-word";
     cerita.style.textAlign = "justify";
-    // cerita.createElement = "button";
 
-})
+    const nextBtn = document.createElement("button");
+    nextBtn.textContent = "gas main bang!";
+    nextBtn.className = "start-game";
+    choices.appendChild(nextBtn);
+
+    nextBtn.addEventListener("click", function() {
+        prologue.style.display = "none";
+        nextBtn.style.display = "none";
+
+        // const senjata = document.createElement("p");
+        // senjata.innerText = isiTeks[0];
+        // cerita.appendChild(senjata);
+    });
+});
+
+
+
+
+
+
 
