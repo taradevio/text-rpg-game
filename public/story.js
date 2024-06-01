0779396d840ccf1aefb33ee8555c6f6d9c371fb3
Nanda <nandayavanets@mail.ru>
2024-06-01 13:06:05 +0700

    add text

---
+++ public/story.js
@@ -0,0 +1,11 @@
+export const isiTeks = [
+    {
+        prologue : "Di sebuah kerajaan bernama Aruna, hidup seorang putri cantik bernama Putri Indira. Suatu hari, putri diculik oleh naga jahat bernama Ragnor dan dibawa ke sarangnya di puncak gunung tinggi. Seluruh kerajaan menjadi sedih dan takut. Namun, ada seorang prajurit muda bernama Arjuna yang sangat berani. Dia bersumpah akan menyelamatkan Putri Indira. Arjuna adalah seorang pendekar yang pandai bertarung dan sangat setia kepada kerajaannya.Dengan pedang di tangan dan tekad di hati, Arjuna memulai perjalanannya menuju sarang naga. Dalam perjalanan, dia menghadapi banyak rintangan dan makhluk buas. Tapi Arjuna tidak gentar. Dia tahu bahwa menyelamatkan putri adalah tugasnya.",
+
+        cerita : "Sang pemain kemudian masuk ke dalam kastil, namun usahanya sia-sia."
+    },
+
+    {
+        senjata: "revolver"
+    }
+]
