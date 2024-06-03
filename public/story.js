4ca2796b3713a62bede1387e4b3f340117d63fce
aufklarungjos <aufklarungjos@gmail.com>
2024-06-03 13:05:12 +0000

    add new a object

--- public/story.js
+++ public/story.js
@@ -1,7 +1,15 @@
 export const isiTeks = [
     {
-        prologue : "Di sebuah kerajaan bernama Aruna, hidup seorang putri cantik bernama Putri Indira. Suatu hari, putri diculik oleh naga jahat bernama Ragnor dan dibawa ke sarangnya di puncak gunung tinggi. Seluruh kerajaan menjadi sedih dan takut. Namun, ada seorang prajurit muda bernama Arjuna yang sangat berani. Dia bersumpah akan menyelamatkan Putri Indira. Arjuna adalah seorang pendekar yang pandai bertarung dan sangat setia kepada kerajaannya.Dengan pedang di tangan dan tekad di hati, Arjuna memulai perjalanannya menuju sarang naga. Dalam perjalanan, dia menghadapi banyak rintangan dan makhluk buas. Tapi Arjuna tidak gentar. Dia tahu bahwa menyelamatkan putri adalah tugasnya.",
+        prologue : "Di sebuah kerajaan bernama Aruna, hidup seorang putri cantik bernama Putri Indira. Suatu hari, putri diculik oleh naga jahat bernama Ragnor dan dibawa ke sarangnya di puncak gunung tinggi. Seluruh kerajaan menjadi sedih dan takut. Namun, ada seorang prajurit muda bernama Arjuna yang sangat berani. Dia bersumpah akan menyelamatkan Putri Indira. Arjuna adalah seorang pendekar yang pandai bertarung dan sangat setia kepada kerajaannya.Dengan pedang di tangan dan tekad di hati, Arjuna memulai perjalanannya menuju sarang naga. Dalam perjalanan, dia menghadapi banyak rintangan dan makhluk buas. Tapi Arjuna tidak gentar. Dia tahu bahwa menyelamatkan putri adalah tugasnya."
 
-        cerita : "Sang pemain kemudian masuk ke dalam kastil, namun usahanya sia-sia."
+    },
+
+    {
+        userName: "Arthur",
+        userClass: "Warrior",
+        userHP: "100",
+        userImage: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f57b5304-de59-49e3-b0f9-cc29a2458425/dcp62r1-7681a179-9fc4-4b7a-8d38-7251e564f332.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y1N2I1MzA0LWRlNTktNDllMy1iMGY5LWNjMjlhMjQ1ODQyNVwvZGNwNjJyMS03NjgxYTE3OS05ZmM0LTRiN2EtOGQzOC03MjUxZTU2NGYzMzIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.vh1gJ8VY5QMFYE28RCIKcaR8OXXeenZtvkweuwOJvY0"
     }
+
+
 ]
