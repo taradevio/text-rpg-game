51525c84b4cb8d6e0d85ca4c13772ee6554c5e7b
aufklarungjos <aufklarungjos@gmail.com>
2024-06-03 13:02:51 +0000

    move js files into folder

--- story.js
+++
@@ -1,7 +0,0 @@
-const isiTeks = [
-    {
-        prologue : "Alkisah ada sebuah kastil yang dihuni manusia kerdil bernama Andrej.",
-
-        cerita : "Sang pemain kemudian masuk ke dalam kastil, namun usahanya sia-sia."
-    }
-]
